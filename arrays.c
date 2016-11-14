1780c24e2487b6c613b2e9acb89537c5ddcd94ab
ggao <ggpaue@gmail.com>
2016-11-14 14:06:55 -0800

    11/14 push arrays and pointers

--- arrays.c
+++ arrays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	/*
@@ -32,7 +33,7 @@ int main() {
 		//}
 	}
 	*/
-
+	/*
 	int avg = 0;
 	int sum = 0; 
 	int x = 0;
@@ -50,6 +51,111 @@ int main() {
 
 	avg = sum/20;
 	printf("%d\n", avg);
+	*/
 
+	/*
+	int i, a[10];
+	for(i = 0; i < 10; i++) {
+		a[i] = i + 1;
+	}
+	for(i = 9; i >= 0; i--) {
+		printf("%d\n", a[i]);
+	}
+	*/
+
+	/*
+	int i, j, t, LearnTime[10];
+	printf("please enter 10 number: \n");
+	
+	for(i = 0; i < 10; i++) {
+		scanf("%d", &LearnTime[i]);
+	}
+
+	for(j = 0; j < 9; j++) {
+		for(i = 0; i < 9 - j; i++) {
+			if(LearnTime[i] > LearnTime[i + 1]) {
+				t = LearnTime[i];
+				LearnTime[i] = LearnTime[i + 1];
+				LearnTime[i + 1] = t;
+			}
+		}
+	}
+
+	printf("the sorted number: \n");
+	for(i = 0; i < 10; i++) {
+		printf("%d\t", LearnTime[i]);
+	}
+	*/
+
+	/*
+	int i, j, row = 0, colum = 0, max;
+	int a[3][4] = {{3,4,16,2}, {7,5,1,9}, {11,23,3,8}};
+	max = a[0][0];
+	for(i = 0; i < 3; i++) {
+		for(j = 0; j < 4; j++) {
+			if(a[i][j] > max) {
+				max = a[i][j];
+				row = i;
+				colum = j;
+			}
+		}
+	}
+	printf("max = %d\nrow=%d\ncolum=%d\n", max, row, colum);
+	*/
+
+	/*
+	char c[][5] = {{'*', '*', '*', '*', '*'}, {'*', ' ', ' ', ' ', '*'}, {'*', ' ', ' ', ' ', '*'}, {'*', ' ', ' ', ' ', '*'}, {'*', '*', '*', '*', '*'}};
+	int i, j;
+	for(i = 0; i < 5; i++) {
+		for(j = 0; j < 5; j++) {
+			printf("%c", c[i][j]);
+		}
+		printf("\n");
+	}
+	*/
+	/*
+	char c[] = "Shiyanlou";
+	printf("%s\n", c);
+	printf("%c\n", c[0]);
+	*/
+
+	/*
+	char str[] = "China\nChengdu";
+	puts(str);
+	*/
+	/*
+	char str[100];
+	gets(str);
+	printf("%s\n", str);
+	*/
+
+	/*
+	char str1[30] = "People's Republic of ";
+	char str2[] = "China";
+	printf("%s\n", strcat(str1, str2));
+	printf("%d\n", strlen(str2));
+	*/
+	/*
+	char str1[10], str2[] = "China";
+	strcpy(str1, str2);
+	printf("%s\n", str1);
+
+	strcmp("Chengdu", "Beijing");
+	*/
+
+	char string[30];
+	char ch[3][30];
+	int i;
+	for(i = 0; i < 3; i++) {
+		gets(ch[i]);
+	}	
+	strcpy(string, ch[0]);
+	
+	for(i = 1; i < 3; i++) {
+		if(strcmp(ch[i], string) < 0) {
+			strcpy(string, ch[i]);
+		}
+	}
+	printf("the result is: \n%s\n", string);
 	return 0;
 }
--- pointers.c
+++ pointers.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 
 int var = 1;
+
+void swap(int *point_1, int *point_2) {
+	int temp;
+	temp = *point_1;
+	*point_1 = *point_2;
+	*point_2 = temp;
+}
+
+void swap2(int p, int q) {
+	int temp;
+	temp = p;
+	p = q;
+	q = temp;
+}
+
+void exchange(int *q1, int *q2, int *q3) {
+	void swap(int *pt1, int *pt2);
+	if(*q1 < *q2) swap(q1, q2);
+	if(*q1 < *q3) swap(q1, q3);
+	if(*q2 < *q3) swap(q2, q3);
+}
+
 int main() {
 	/*
 	int num = 10;
 	printf("Values of var is: %d\n", num);
 	printf("Address of value is: %p\n", &num);
 	*/
-
+	/*
 	int *p;
 	int var = 10;
 
 	p = &var;
-
+	*/
 
 	//printf("Value of var is: %d\n", var);
 	//printf("Address of var is: %p\n", p);
@@ -29,7 +51,7 @@ int main() {
 	printf("Value of var is: %d\n", *p);
 	printf("Value of var is: %d\n", *(&var));
 	*/
-
+	/*
 	char ch = 'a';
 	char *pr;
 	pr = &ch;
@@ -37,6 +59,48 @@ int main() {
 	*pr = 'b';
 	printf("Value of ch: %c\n", ch);
 	printf("Value of ch: %c\n", *pr);
+	*/
+
+	/*
+	int a = 100, b = 10;
+	int *pointer_1, *pointer_2;
+	pointer_1 = &a;
+	pointer_2 = &b;
+	printf("a = %d, b = %d\n", a, b);
+	printf("%ld, %ld\n", pointer_1, pointer_2);
+	printf("*pointer1=%d, *pointer_2=%d\n", *pointer_1, *pointer_2);
+	*/
+	/*
+	int *p1, *p2, *p, a, b;
+	printf("please enter two integer number: ");
+	scanf("%d,%d", &a, &b);
+	//printf("a=%d,b=%d\n", a, b);
+
+	p1 = &a;
+	p2 = &b;
+
+	swap2(a, b);
+	printf("a=%d,b=%d\n", a, b);
+
+	if(a < b) {
+		//p = p1;
+		//p1 = p2;
+		//p2 = p;
+		swap(p1, p2);
+	}
+
+	printf("a=%d,b=%d\n", a, b);
+	printf("max=%d,min=%d\n", *p1, *p2);
+	*/
+
+	int a,b,c,*p1,*p2,*p3;
+	printf("please enter 3 integer number:");
+	scanf("%d%d%d", &a, &b, &c);
+	p1=&a;
+	p2=&b;
+	p3=&c;
+	exchange(p1,p2,p3);
+	printf("the order is: %d,%d,%d\n", a, b, c);
 
 	return 0;
 }
