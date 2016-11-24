bd00fe8820779701badbdbb4e9f411d8ef1234c8
ggao <ggpaue@gmail.com>
2016-11-23 17:48:41 -0800

    save and delete function

--- contacts.c
+++ contacts.c
@@ -212,7 +212,7 @@ int save(NODE *head) {
 	}
 
 	while(p1 != NULL) {
-		fwrite(p1, sizeof(p1 -> info), 1, fp);
+		fwrite(p1, sizeof(p1 -> info.name), 1, fp);
 		p1 = p1 -> next;
 	}
 
@@ -221,6 +221,152 @@ int save(NODE *head) {
 	return 0;
 }
 
+int del(NODE *head) {
+	if(head -> next == NULL) {
+		printf("the list is empty! \n");
+		printf("\n");
+		return 0;
+	}
+
+	NODE *p1, *p2;
+	p1 = head -> next;
+	p2 = head;
+	printf("search by name or phone? \n");
+	fflush(stdin);
+	char ch = getchar();
+	if(ch == 'n') {
+		printf("please input name you want to search: \n");
+		
+		char na[16];
+		fflush(stdin);
+		gets(na);
+
+		while(p1 != NULL) {
+			if(strcmp(p1 -> info.name, na) == 0) {
+				printf("%s %s\n", p1 -> info.name, p1 -> info.phone);
+				printf("are you sure to delete this entry? \n");
+				ch = getchar();
+				if(ch == 'Y' || ch == 'y') {
+					NODE *p3 = (NODE *)malloc(sizeof(NODE));
+					p3 = p1;
+					p2 -> next = p1 -> next;
+					p1 = p1 -> next;
+					free(p3);
+				}
+			}
+		
+			p2 = p1;
+			p1 = p1 -> next;
+		}
+		return 0;
+
+	} else if(ch == 'p') {
+		printf("please input phone number you want to search: \n");
+		char tele[32];
+		fflush(stdin);
+		gets(tele);
+
+		while(p1 != NULL) {
+			if(strcmp(p1 -> info.phone, tele) == 0) {
+				printf("\n");
+				printf("%s  %s\n", p1 -> info.name, p1 -> info.phone);
+				printf("are you sure to delete this entry? \n");
+				
+				ch = getchar();
+				if(ch == 'y') {
+					p2 -> next = p1 -> next;
+					p1 = p1 -> next;
+				} else {
+					return 0;
+				}
+			}
+
+			p2 = p1;
+			p1 = p1->next;
+		}
+		return 0;
+	}
+	return 0;
+}
+
+int freelist(NODE *head) {
+	NODE *p1, *p2;
+	p1 = p2 = head;
+
+	if(head -> next == NULL) {
+		free(head);
+		return 0;
+	}
+
+	p1 = p1 -> next;
+	while(p1 != NULL) {
+		free(p2);
+		p2 = p1;
+		p1 = p1->next;
+	}
+
+	free(p2);
+	return 0;
+}
+
+/*
+int clearlist(NODE *head) {
+	int i = 0;
+	NODE *p1, *p2;
+	p1 = p2 = head -> next;
+	
+	while(p2 != NULL) {
+		i = 1;
+		p1 = p1 -> next;
+		free(p2);
+		p2 = p1;
+	}
+
+	if(i) head -> next = NULL;
+	return i;
+}
+*/
+
+void menu() {
+	NODE *head;
+
+	head = load();
+
+	while(1) {
+		if(head == NULL) {
+			printf("can not read file. \n");
+			break;
+		} else {
+			printf("data load successfully. \n");
+		}
+	}
+
+	while(1) {
+		printf("please select the following options: \n");
+		printf("(1) view the contact list\n" );
+		printf("(2) search for a contact. \n");
+		printf("(3) add a new contact. \n");
+		printf("(4) delete a contact. \n");
+		printf("(5) save the contact list. \n");
+		printf("(6) exit the system.\n" );
+		
+
+		int s;
+		scanf("%d", &s);
+
+		switch(s) {
+			case 1: view(head); break;
+			case 2: search(head); break;
+			case 3: add(head); break;
+			case 4: del(head); break;
+			case 5: save(head); break;
+			default: printf("error! please input again!\n");
+		}
+
+	}
+}
+
 void main() {
+	menu();
 	
 }
